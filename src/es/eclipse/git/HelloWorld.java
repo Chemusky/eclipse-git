9ae5ea25a456a6b401777e7fbd2e984530e07394
José Miguel <Tapuy@hotmail.es>
2025-05-16 12:32:42 +0200

    feat: add HelloWorld class

---
+++ src/es/eclipse/git/HelloWorld.java
@@ -0,0 +1,15 @@
+package es.eclipse.git;
+
+public class HelloWorld {
+	static String name = "José Miguel";
+	
+	static String hello() {
+		return "Hello World " + name;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(hello());
+
+	}
+
+}
